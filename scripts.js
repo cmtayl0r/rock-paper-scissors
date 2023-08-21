ae65f17e3205742b1911b931d643597c76840ba0
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-08-21 14:45:11 +0200

    add playround logic for round winner

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ const scorePEl = document.querySelector('.score__pla');
 const scoreCEl = document.querySelector('.score__com');
 const roundEl = document.querySelector('.round');
 const updateEl = document.querySelector('.update');
+const choicePEl = document.querySelector('.choice__pla');
+const choiceCEl = document.querySelector('.choice__com');
 
 /* 
 ---------------------------------
@@ -27,8 +29,20 @@ const init = function () {
     // scores array (player 0, Computer 1)
     scores = [0, 0];
     // round #
-    round = 0;
+    round = 1;
+
+    // Reset scores
+    scorePEl.textContent = `🧑 has ${scores[0]}`;
+    scoreCEl.textContent = `🤖 has ${scores[0]}`;
+    // Set round # to round variable
+    roundEl.textContent = round;
+    // Set update to generic start statement
+    updateEl.textContent = 'Make a choice!';
+    // Set display choices to ?
+    choicePEl.textContent = '?';
+    choiceCEl.textContent = '?';
 };
+init();
 
 /* 
 ---------------------------------
@@ -53,21 +67,49 @@ console.log(getComputerChoice());
 */
 
 // - add event listeners to the 3 button DOM choices
-// - getPlayerChoice [Function] t eliminate repeating code on 3 buttons
+// - getPlayerChoice [Function] to eliminate repeating code on 3 buttons
 // - connect choice selection to array value (choices)
 // - trigger game function [Function]
 // - return user choice
 
-
-
-
-
-
-
 // PLAYER CHOICE
-const getPlayerChoice = function () {};
+// const getPlayerChoice = function () {};
 
-// 5. playRound [Function]
+const playerTemp = 'Rock';
+
+/* 
+---------------------------------
+4. PLAY ROUND
+---------------------------------
+*/
+const playRound = function (pChoice, cChoice) {
+    // rock > scissors, scissors > paper, paper > rock
+    if (
+        (pChoice === choices[0] && cChoice === choices[2]) ||
+        (pChoice === choices[2] && cChoice === [1]) ||
+        (pChoice === choices[1] && cChoice === choices[0])
+    ) {
+        console.log(`🧑 Player wins with ${pChoice} vs ${cChoice}`);
+        scores[0]++;
+    } else if (
+        (cChoice === choices[0] && pChoice === choices[2]) ||
+        (cChoice === choices[2] && pChoice === [1]) ||
+        (cChoice === choices[1] && pChoice === choices[0])
+    ) {
+        console.log(`🤖 Computer wins with ${cChoice} vs ${pChoice}`);
+        scores[1]++;
+    } else {
+        console.log('Its a draw 👍');
+    }
+};
+
+playRound(playerTemp, getComputerChoice());
+playRound(playerTemp, getComputerChoice());
+playRound(playerTemp, getComputerChoice());
+playRound(playerTemp, getComputerChoice());
+playRound(playerTemp, getComputerChoice());
+console.log(scores);
+
 // - Input parameters for player and computer choices
 // - "Rock' paper, Scissors" countdown - https://css-tricks.com/how-to-create-an-animated-countdown-timer-with-html-css-and-javascript/
 // - Evaluate parameters
